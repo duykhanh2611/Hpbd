83ce71a0d590da985db60620d677e2234178ebde
duykhanh2611 <lawesss2611@gmail.com>
2025-11-21 12:55:23 +0700

    Initial commit with Dockerfile

---
+++ WebApplication1/wwwroot/js/hearts.js
@@ -0,0 +1,10 @@
+﻿setInterval(() => {
+    const heart = document.createElement("div");
+    heart.className = "heart";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = (Math.random() * 20 + 20) + "px";
+    heart.innerText = "❤️";
+    document.body.appendChild(heart);
+
+    setTimeout(() => heart.remove(), 4000);
+}, 300);
